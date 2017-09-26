7c43ce6bada91ab24fb42ecce0cde92e753b3e9b
DARJIYO <djarjiyocute@gmail.com>
2017-09-27 01:24:17 +0700

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/motorola/victara/full_victara.mk)
+$(call inherit-product, device/lenovo/a6000/full_a6000.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_RELEASE_NAME := MOTO X (2014)
-PRODUCT_NAME := cm_victara
+PRODUCT_RELEASE_NAME := lenovo
+PRODUCT_NAME := a6000
 
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
